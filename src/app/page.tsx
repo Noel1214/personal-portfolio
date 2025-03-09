0070912d3e36ec00f00345d825fa2e9e6ba46e73
Noel Sebu <noelsebu555@gmail.com>
2025-03-09 21:53:02 +0530

    set up typed js and added string

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
+'use client'
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 const page = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["I am a MERN stack developer."],
+      typeSpeed: 40,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="overflow-x-hidden">
-      <div><h1 className="text-xl m-5 absolute right-4">Resume</h1></div>
+      <div>
+        <h1 className="text-xl m-5 absolute right-4">Resume</h1>
+      </div>
       <div className="w-screen h-screen ml-[8rem] mr-[2rem] flex items-center">
-        <div className="min-w-[44rem] flex items-center justify-between -translate-y-6 break-words">
+        <div className="min-w-[45rem] flex items-center gap-5 -translate-y-6 break-words">
           {/* Profile image  */}
-          <div>
+          <div className="w-[20rem]">
             <Image
               className="w-[20rem] h-[20rem] scale-90 object-cover rounded-full overflow-hidden"
               src="/Noel.jpg"
@@ -18,10 +35,11 @@ const page = () => {
             />
           </div>
           {/* Name */}
-          <div className="text-[27px] font-se">
-            <h3>Hi my name is</h3>
-            <h1 className="text-3xl font-bold">Noel Sebu</h1>
-            <h3>I am a MERN stack developer</h3>
+          <div className="w-auto text-[27px]">
+            <h3>Hi, my name is</h3>
+            <h1 className="text-5xl font-semibold">Noel Sebu</h1>
+            <span ref={el} className="py-4 h-6">I</span>
+
           </div>
         </div>
       </div>
